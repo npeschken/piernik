aec87f066171fd1bd7c9b005559a601d39bc5bed
npeschken <npeschken@camk.edu.pl>
2026-07-07 11:01:11 +0200

    [IO] updating IO for CR streaming

--- src/IO/hdf5/restart_hdf5_v1.F90
+++ src/IO/hdf5/restart_hdf5_v1.F90
@@ -268,9 +268,7 @@ contains
          call h5pset_dxpl_mpio_f(plist_id, H5FD_MPIO_INDEPENDENT_F, error)
       else
 #endif /* INDEPENDENT_ATOUTB */
-         if (.not. is_multicg) then
-            call h5pset_dxpl_mpio_f(plist_id, H5FD_MPIO_COLLECTIVE_F, error)
-         endif
+         if (.not. is_multicg) call h5pset_dxpl_mpio_f(plist_id, H5FD_MPIO_COLLECTIVE_F, error)
 #ifdef INDEPENDENT_ATOUTB
       endif
 #endif /* INDEPENDENT_ATOUTB */
